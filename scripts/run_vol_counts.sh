9e1eefa0e11b5873a3e214c243db8dee8fe005c1
Kendra Frederick <kendra.frederick@travelport.com>
2022-09-07 08:54:53 -1000

    updated script; recent notebooks

--- scripts/run_vol_counts.sh
+++ scripts/run_vol_counts.sh
@@ -9,5 +9,4 @@ nohup spark-submit \
     --conf spark.pyspark.python=python2 \
     --conf spark.pyspark.driver.python=python2 \
     --conf spark.sql.shuffle.partitions=8 \
-    vol_counts_markets_destinations.py --run-mode spark-submit --shop-start 2022-08-26 --shop-end 2022-08-31 > kf-vol-counts-out.txt 2> kf-vol-counts-err.txt | tee &
-
+    vol_counts_markets.py --run-mode spark-submit --shop-start 2022-08-19 --shop-end 2022-0906 > kf-vol-counts-out.txt 2> kf-vol-counts-err.txt | tee &
