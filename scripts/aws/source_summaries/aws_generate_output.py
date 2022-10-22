b3814ecc250b345db9b089116964bfd9f12b53af
Kendra Frederick <kendra.frederick@travelport.com>
2022-10-21 14:15:00 -1000

    reorg AWS scripts

---
+++ scripts/aws/source_summaries/aws_generate_output.py
@@ -0,0 +1,366 @@
+"""
+Generates output file for upload to AWS, for use in Calendar
+API. Saves as csv.
+
+Applied restricts or filters to data based on various empirical
+considerations in order to keep the accuracy of the estimations
+reasonable.
+
+Note that this script is designed to run on aggregated, processed
+data derived from the "datalake" format of raw eStreaming data in 
+AWS.
+
+Notes for updating to Lead Price Summaries:
+- remove shop_counts_threshold
+"""
+
+
+import datetime
+import argparse
+import os
+import math
+
+
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
+import pyspark.sql.types as T
+from pyspark.sql.window import Window
+
+# import numpy as np
+
+cols_to_write = [
+ 'point_of_sale',
+ 'currency',
+ 'origin_city',
+ 'destination_city',
+ 'out_departure_date',
+ 'in_departure_date ',
+ 'min_fare'
+]
+# output_dir = "/tmp/calendar_data"
+BASE_OUTPUT_DIR = "s3://tvlp-ds-lead-price-predictions"
+BASE_INPUT_DIR = "s3://kendra-frederick/shopping-grid/processed-agg-data"
+
+
+script_start_time = datetime.datetime.now()
+print("*****************************")
+print("{} - Starting Lookup File Generation Script".format(script_start_time.strftime("%Y-%m-%d %H:%M")))
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Generate lookup / 'prediction' file. Optionally applies "
+        " filters restrictions to data."
+    )
+    parser.add_argument(
+        "--pos",
+        help="Point of Sale two-letter code",
+        default="US",
+    )
+    parser.add_argument(
+        "--currency",
+        help="Currency (3-letter code)",
+        default="USD",
+    )
+    parser.add_argument(
+        "--stale-after",
+        help="Exclude data older than this many days old. "
+        "If this param is set to 0, restriction will not be applied. Default = 0",
+        type=int,
+        default=0,
+    )
+    parser.add_argument(
+        "--prev-val-ratio",
+        help="Ratio of current value to previous. If data is > than this "
+        "filter it will be excluded. If this param is set to 0, filter "
+        "will not be applied. Default = 0",
+        type=int,
+        default=0,
+    )
+    parser.add_argument(
+        "--ratio-median",
+        help="Ratio of current value to median for market. If data is > than "
+        "this value, it will be excluded. If this param is set to 0, filter "
+        "will not be applied. Default = 0",
+        type=int,
+        default=0,
+    )
+    parser.add_argument(
+        "--ratio-mean",
+        help="Ratio of current value to mean for market. If data is > than  "
+        "this value, it will be excluded. If this param is set to 0, filter "
+        "will not be applied. Default = 0",
+        type=int,
+        default=0,
+    )
+    parser.add_argument(
+        "--shop-counts",
+        help="Minimum threshold applied to shop counts in conjuction with mean "
+        "and/or median ratios. If this param is set to 0, "
+        "filter will not be applied. Default = 0",
+        type=int,
+        default=0,
+    )
+    parser.add_argument(
+        "--dow-filter",
+        help="Wheter to apply day of week (DOW) combination requirement to "
+        "a market. If market has not been shopped for all combinations of "
+        "DOW of travel in data, it will be excluded.",
+        action="store_true",
+        default=False,
+    )
+    parser.add_argument(
+        "--rsd",
+        help="Relative standard deviation (RSD) reqirement by market. "
+        "If market's RSD data is > than this value, it will be excluded. "
+        "If this param is set to 0, filter will not be applied. Default = 0",
+        type=int,
+        default=0,
+    )
+    parser.add_argument(
+        "--rank",
+        help="Filter out market outside the top rank by volume. "
+        "If this param is set to 0, filter will not be applied. Default = 0",
+        type=int,
+        default=0,
+    )
+    args = parser.parse_args()
+    return args
+
+
+APP_NAME = "generate-calendar-predictions"
+
+# ===========================
+# HELPER FUNCS
+# ===========================
+
+def add_dow_ind(df):
+    ## add in DOW indicators
+    df = (df
+        .withColumn("dept_dt_dow_int", (F.date_format("outDeptDt_dt", "u") - 1).cast(T.IntegerType()))
+        .withColumn("dept_dt_dow", F.date_format("outDeptDt_dt", "E"))
+        .withColumn("ret_dt_dow_int", (F.date_format("inDeptDt_dt", "u") - 1).cast(T.IntegerType()))
+        .withColumn("ret_dt_dow", F.date_format("inDeptDt_dt", "E"))
+        )
+    return df
+
+
+def filter_prev_val_ratio(df, prev_val_ratio):
+    """Filter out data based on its ratio to the previous value."""
+    pass
+
+
+def filter_ratio_median(df, ratio_median, shop_counts_threshold=0):
+    """Filter out data based on ratio to its markets median lowest fare.
+
+    An anomaly is defined as being higher than X-fold of the median
+    value for that market's min fares *and* having shop counts below
+    a threshold. We exclude that datapoint from further calculations.
+    """
+    w = Window.partitionBy("market")
+    df = df.withColumn(
+        "median", F.expr("percentile_approx(min_fare, 0.5)").over(w)
+    )
+    df = df.withColumn(
+        "ratio_median", 
+        F.col("min_fare") / F.col("median")
+    )
+    if shop_counts_threshold > 0:
+        df_filt_anom = df.filter(
+                ~((F.col("ratio_median") >= ratio_median)
+                & (F.col("shop_counts") < shop_counts_threshold))
+                )
+    else:
+         df_filt_anom = df.filter(
+                (F.col("ratio_median") < ratio_median)
+         )
+                  
+    return df_filt_anom
+
+
+def filter_ratio_mean(df, ratio_mean, shop_counts_threshold):
+    """Filter out data based on its ratio to its market's mean lowest fare.
+
+    Only filters out high-fare anomalies.
+    """
+    w = Window.partitionBy("market")
+    df = df.withColumn(
+        "mean", F.mean("min_fare").over(w)
+    )
+    df = df.withColumn(
+        "ratio_mean", 
+        F.col("min_fare") / F.col("mean")
+    )
+    if shop_counts_threshold > 0:
+        df_filt_anom = df.filter(
+                ~((F.col("ratio_mean") >= ratio_mean)
+                & (F.col("shop_counts") < shop_counts_threshold))
+                )
+    else:
+         df_filt_anom = df.filter(
+                (F.col("ratio_mean") < ratio_mean)
+         )
+    return df_filt_anom
+
+
+def filter_rsd(df, rsd_threshold):
+    """Filter out market based on its RSD.
+
+    RSD = relative standard deviation = stddev / mean
+
+    Note: these calcs should typically be done after any anomaly filtering
+    """
+    market_fare_stats = (df
+                        .groupBy("market")
+                        .agg(
+                            F.mean("min_fare").alias("avg_min_fare"),
+                            F.stddev("min_fare").alias("std_min_fare"),
+                            # F.min("min_fare").alias("min_min_fare"),
+                            # F.mean("min_fare").alias("max_min_fare"),
+                        )
+                    .withColumn("rsd", F.col("std_min_fare")/F.col("avg_min_fare"))
+                    .dropna()
+                        )
+    rsd_markets_to_keep = market_fare_stats.filter(F.col('rsd') < rsd_threshold).select('market')
+    df_filt_rsd = df.join(rsd_markets_to_keep, on='market', how='inner')
+    return df_filt_rsd
+
+
+def filter_dow_combo(df, combo_count=49):
+    """Filter out markets that do not have all DOW combo's of travel
+
+    DOW = days of week. All combinations refers to departure date and return
+    dates of travel.
+    """
+    mrkt_dow_cnts = df.groupBy("market", "dept_dt_dow_int", "ret_dt_dow_int").count()
+    mrkt_dow_cnts2 = mrkt_dow_cnts.groupBy("market").count()
+
+    markets_all_dow = mrkt_dow_cnts2[mrkt_dow_cnts2['count'] == combo_count]
+    df_filt_dow = df.join(markets_all_dow.select("market"), on="market", how='inner')
+    return df_filt_dow
+
+
+def filter_rank(df, rank):
+    """Filter markets based on volume rank."""
+    top_rank = rank
+
+    market_cnts = (df
+                    .groupBy("market")
+                    .agg(F.sum('shop_counts').alias("sum_shop_counts"))
+    )
+
+    w_ord = Window.orderBy(F.desc("sum_shop_counts"))
+    market_cnts = market_cnts.withColumn("rank", F.row_number().over(w_ord))
+
+    markets_filt = market_cnts[market_cnts['rank'] <= top_rank]
+    df_filt_top = df.join(markets_filt.select("market", "rank"), on="market")
+    return df_filt_top
+    
+# ===========================
+# MAIN
+# ===========================
+
+if __name__ == "__main__":
+    args = parse_args()
+    prev_val_ratio = args.prev_val_ratio
+    ratio_median = args.ratio_median
+    ratio_mean = args.ratio_mean
+    shop_counts_threshold = args.shop_counts
+    dow_filter = args.dow_filter
+    rsd = args.rsd
+    rank = args.rank
+    stale_after = args.stale_after
+
+    pos = args.pos
+    currency = args.currency
+
+    input_dir = "{}/{}-{}".format(BASE_INPUT_DIR, pos, currency)
+    output_dir = "{}/{}-{}".format(BASE_OUTPUT_DIR, pos, currency)
+
+    print("Processing POS: {}, currency: {}".format(pos, currency))
+
+    no_filters = (
+        (prev_val_ratio == 0)
+        & (ratio_median == 0)
+        & (ratio_mean == 0)
+        & (dow_filter is False)
+        & (rsd == 0)
+        & (rank == 0)
+    )
+
+    # derive shopping date cutoff
+    # assumes input data is current through today / yesterday
+    shop_date_cutoff = datetime.date.today() - datetime.timedelta(days=stale_after)
+    # shop_date_cutoff_int = int(shop_date_cutoff.strftime("%Y%m%d"))
+
+    spark = SparkSession.builder.appName(APP_NAME).getOrCreate()
+
+    # LOAD DATA
+    print("Reading data")
+    df = spark.read.parquet(input_dir)
+
+    if stale_after > 0:
+        df = df.filter(F.col("searchDt_dt") >= shop_date_cutoff)
+
+    # modify "market"
+        # prior: defined by airport
+        # now: defined by city
+    df = df.withColumn("market", 
+            F.concat_ws("-", F.col("origin_city"), F.col("destination_city"))
+    )
+    df.cache()
+
+    print("Applying filters & restrctions")
+    df_mod = df.select("*")
+
+    if dow_filter:
+        df = add_dow_ind(df)
+        df_mod = filter_dow_combo(df_mod)
+
+    if ratio_mean != 0:
+        df_mod = filter_ratio_mean(
+            df_mod, ratio_mean, shop_counts_threshold
+        )
+
+    if ratio_median != 0:
+        df_mod = filter_ratio_median(
+            df_mod, ratio_median, shop_counts_threshold
+        )
+
+    if rsd != 0:
+        df_mod = filter_rsd(df_mod, rsd)
+
+    if rank != 0:
+        df_mod = filter_rank(df_mod, rank)
+
+
+    # select most recent
+    w = (Window
+            .partitionBy('market', 'out_departure_date', 'in_departure_date')
+            .orderBy(F.desc("search_date"))
+            )
+    df_with_recency = (df_mod
+                    .withColumn("recency_rank", F.row_number().over(w))
+                    )
+    df_most_recent = df_with_recency.filter(F.col("recency_rank") == 1)
+
+    n = df_most_recent.count()
+
+    print("Output data size: {}".format(n))
+
+    # Want < 500,000 records per file
+    num_parts = math.ceil(n/5e5) 
+
+    # write to HDFS csv
+    print("Writing data")
+    (df_most_recent
+    .select(cols_to_write)
+    .coalesce(num_parts) # haven't seen a need to convert this to repartition yet...
+    .write.mode("overwrite")
+    .csv(output_dir, header=True)
+    )
+
+    script_end_time = datetime.datetime.now()
+    elapsed_time = (script_end_time - script_start_time).total_seconds() / 60   
+    print("Done with output script - Total elapsed time: {:.02f} minutes".format(elapsed_time))
+    print("*****************************")
+    print()
---
+++ scripts/aws/source_summaries/aws_preprocess.py
@@ -0,0 +1,279 @@
+"""
+Pre-processes summarized eStreaming / air shopping data in support of 
+downstream processing related to modeling, caching, and analysis.
+
+- filters on POS and currency
+    - O or D in POS country
+- converts data types
+- creates of 'days until departure' (dtd) and 'stay duration' 
+    (or length of stay, LOS) features and filters on them
+
+Notes / thoughts:
+- In the future, we may want to *not* filter on dtd and LOS here, 
+    but rather keep data less restricted
+"""
+
+import os
+import datetime
+import argparse
+
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
+import pyspark.sql.types as T 
+
+
+APP_NAME = "KF-ProcessAggdShopData"
+
+BASE_INPUT_DIR = "s3://tvlp-ds-lead-price-archives"
+BASE_OUTPUT_DIR = "s3://kendra-frederick/shopping-grid/processed-agg-data"
+AIRPORT_LOOKUP_PATH = "s3://kendra-frederick/reference-data/AIRPORT.CSV"
+
+DATE_FORMAT = "%Y-%m-%d"
+
+
+
+# ========================
+# HELPER FUNCTIONS
+# ========================
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--shop-start",
+        help="Date of shopping data to begin processing at. "
+        "Format YYYY-MM-DD.",
+        type=str,
+        required=True
+    )
+    parser.add_argument(
+        "--shop-end",
+        help="Date of shopping data to end processing at (inclusive). "
+        "Format YYYY-MM-DD.",
+        type=str,
+        required=True
+    )
+    parser.add_argument(
+        "--min-stay-duration",
+        help="Minimum length of stay / stay duration",
+        type=int,
+        default=0,
+    )
+    parser.add_argument(
+        "--max-stay-duration",
+        help="Maximum length of stay (LOS)/ stay duration. Note "
+        "this should take into account the grid size. e.g., if the "
+        "grid is 7x7 (departure and return date each +/- 3 days) and the max "
+        "LOS allowed (difference between departure date & return data) is 7 "
+        "days, then the max stay duration = 7 + 3 + 3 = 13",
+        type=int,
+        default=21,
+    )
+    parser.add_argument(
+        "--min-days-til-dept",
+        help="Minimum days til departure",
+        type=int,
+        default=0,
+    )
+    parser.add_argument(
+        "--max-days-til-dept",
+        help="Maximum days til departure",
+        type=int,
+        default=120,
+    )
+    parser.add_argument(
+        "--pos",
+        help="Point of Sale two-letter code",
+        default="US",
+    )
+    parser.add_argument(
+        "--currency",
+        help="Currency (3-letter code)",
+        default="USD",
+    )
+    parser.add_argument(
+        "--test", "-t",
+        help="Run in test mode",
+        default=False,
+        action="store_true"
+    )
+    parser.add_argument(
+        "--write-mode", "-wm",
+        help="Write mode (e.g. 'append' or 'overwrite')",
+        choices=("append", "overwrite", "ignore"),
+        default="append",
+    )
+
+    args = parser.parse_args()
+    return args
+
+
+def preprocess_data(df, min_days_til_dept, max_days_til_dept,
+    min_stay_duration, max_stay_duration
+    ):
+    """Preprocesses data
+    
+    - Computes stay duration and days until departure
+    - Filter data on:
+        - stay duration between min/max
+        - days_til_dept between min/max
+        - round trip or one-way intineraries
+    """
+    df_proc = (df
+        .withColumn("market", 
+            F.concat_ws("-", F.col("origin"), 
+                                F.col("destination"))
+                    )
+    #     .withColumnRenamed("price", "min_fare")
+        .withColumnRenamed("date", "departure_date")
+        .withColumn("shop_date", F.concat_ws("-", F.col("year"), F.col("month"), F.col("day")))
+        .withColumn("departure_date_dt", F.to_date("departure_date", "yyyy-MM-dd"))
+        .withColumn("shop_date_dt", F.to_date("shop_date", "yyyy-MM-dd"))
+        .withColumn('days_til_dept', F.datediff(
+                    F.col('departure_date_dt'), F.col('shop_date_dt'))
+                )
+    )
+    
+    # Summary data has already been filtered for min/max days til departure & LOS
+    # Besides, we can filter on them if needed, downstream in other uses of this data
+    # df_filt = (df_proc
+    #             .filter(F.col('days_til_dept').between(
+    #                             min_days_til_dept, max_days_til_dept)
+    #                     )
+    #             .filter((F.col("los").between(
+    #                     min_stay_duration, max_stay_duration))
+    #                     # Summary data does not contain one-way's
+    #                 # | (F.col("los").isNull())
+    #                 )
+    #         )
+
+    # add shop indicator for individual market heatmaps
+    # df_filt = df_filt.withColumn("shop_ind", F.lit(1))
+    
+    # return df_filt
+    return df_proc
+
+
+def filter_pos(df, pos, currency):
+    """Filters on pos and currency, and also O & D in pos country.
+    
+    Also enriches data with O & D city.
+    """
+    pos_df = df.filter(
+        (F.col("pos") == pos) & (F.col("currency") == currency)
+    )
+
+    # enrich with O, D country
+    pos_enr_df = (pos_df
+                .join(
+                    city_df,
+                    on=[pos_df['origin'] == city_df['reference_city_code']],
+                    how='left'
+                )
+                .withColumnRenamed("country_code", "origin_country")
+                .drop("reference_city_code")
+                )
+    pos_enr_df = (pos_enr_df
+                .join(
+                    city_df,
+                    on=[pos_df['destination'] == city_df['reference_city_code']],
+                    how='left'
+                )
+                .withColumnRenamed("country_code", "destination_country")
+                .drop("reference_city_code")
+                )
+    # filter on O or D in country of POS
+    pos_enr_filt_df = pos_enr_df.filter(
+        (F.col('origin_country') == pos) | (F.col("destination_country") == pos)
+    )
+    return pos_enr_filt_df
+
+
+def load_process_write_data(date, spark):
+    # LOAD AGGREGATED ESTREAMING DATA
+    # ----------------------------
+    date_str = date.strftime("%Y%m%d")
+
+    # Datalake format is "decoded"
+    print("Reading data")
+    # input_dir = os.path.join(BASE_INPUT_DIR, date_str)
+    y = date.year
+    m = date.month
+    d = date.day
+    df = spark.read.parquet(BASE_INPUT_DIR).filter(
+        (F.col("year") == y) & (F.col("month") == m) & (F.col("day") == d)
+    )
+
+    # PROCESS DATA
+    # ----------------------------
+    print("Processing data")
+    pos_df = filter_pos(df, pos, currency)
+    proc_df = preprocess_data(pos_df, min_days_til_dept, max_days_til_dept,
+        min_stay_duration, max_stay_duration)
+
+    print("Writing data")
+    output_dir = "{}/{}-{}".format(BASE_OUTPUT_DIR, pos, currency)
+    if test:
+        output_dir += "-test"
+    (proc_df
+        .repartition(1)
+        .write
+        .partitionBy("searchDt")
+        .mode(write_mode)
+        .parquet(output_dir)
+    )
+
+
+if __name__ == "__main__":
+    script_start_time = datetime.datetime.now()
+
+    print("*****************************")
+    print("{} - Starting Preprocessing Script".format(script_start_time.strftime("%Y-%m-%d %H:%M")))
+    
+    # PARSE ARGS
+    # ------------------------
+    args = parse_args()
+    min_stay_duration = args.min_stay_duration
+    # add one to allow for shifted features
+    max_stay_duration = args.max_stay_duration + 1 
+    min_days_til_dept = args.min_days_til_dept
+    # add one to allow for shifted features
+    max_days_til_dept = args.max_days_til_dept + 1
+
+    shop_start_str = args.shop_start
+    shop_end_str = args.shop_end
+    shop_start_dt = datetime.datetime.strptime(shop_start_str, DATE_FORMAT).date()
+    shop_end_dt = datetime.datetime.strptime(shop_end_str, DATE_FORMAT).date()
+    num_shop_days = (shop_end_dt - shop_start_dt).days
+    shop_date_range = {shop_start_dt + datetime.timedelta(days=d) for d in range(num_shop_days)}
+    shop_str_range = [d.strftime("%Y%m%d") for d in shop_date_range]
+    shop_int_range = [int(s) for s in shop_str_range]
+
+    pos = args.pos
+    currency = args.currency
+
+    write_mode = args.write_mode
+
+    test = args.test
+
+    print("Script arguments / params: {}".format(args))
+    print("Saving processed data to: {}".format(BASE_OUTPUT_DIR))
+
+    num_days = (shop_end_dt - shop_start_dt).days
+    date_list = [shop_start_dt + datetime.timedelta(days=x) for x in range(num_days + 1)]
+
+    # SET UP SPARK
+    spark = SparkSession.builder.appName(APP_NAME).getOrCreate()
+
+    # Load & massage lookup table
+    airport_df = spark.read.option("header", "true").csv(AIRPORT_LOOKUP_PATH)
+    city_df = airport_df.select("reference_city_code", "country_code").drop_duplicates()
+
+    for date in date_list:
+        load_process_write_data(date, spark)
+
+    script_end_time = datetime.datetime.now()
+    elapsed_time = (script_end_time - script_start_time).total_seconds() / 60
+    print("Done with preprocessing - Total elapsed time: {:.02f} minutes".format(elapsed_time))
+    print("*****************************")
+    print("")
+    spark.stop()
